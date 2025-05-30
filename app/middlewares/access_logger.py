6c7b433949ef680c1cd0ad09799313f01765ed49
sokritha <sokritha.dev@gmail.com>
2025-05-30 07:45:11 +0700

    fix: change access log to get the http header like ip accessor

--- app/middlewares/access_logger.py
+++ app/middlewares/access_logger.py
@@ -7,7 +7,13 @@ logger = logging.getLogger("access")
 
 class AccessLoggingMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        ip = request.client.host
+        x_forwarded_for = request.headers.get("x-forwarded-for")
+        ip = (
+            x_forwarded_for.split(",")[0].strip()
+            if x_forwarded_for
+            else request.client.host
+        )
+
         path = request.url.path
         method = request.method
 
